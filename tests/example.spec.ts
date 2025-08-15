81961ee14c32ba81efdc7d3802f6e5af3d5338c2
Nga Tran <quynhnga610@gmail.com>
2025-08-15 10:59:18 +0700

    Fix comment in test case example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,15 +4,15 @@ test('homework_lession1_ngattq', async ({ page }) => {
   await page.goto('https://material.playwrightvn.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/"Tài liệu học automation test"/);
+  await expect(page).toHaveTitle(/Tài liệu học automation test/);
 });
 
-test('get started link', async ({ page }) => {
+test('link bai hoc so 1', async ({ page }) => {
   await page.goto('https://material.playwrightvn.com/');
 
-  // Click the get started link.
+  // Click link bai hoc so 1
   await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
 
-  // Expects page to have a heading with the name of Installation.
+  // Expects page to have a heading with the name of User Registration.
   await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
 });
